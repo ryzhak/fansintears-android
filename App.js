1954a82bb9f067817202bdb1be4e00d2fff350db
ryzhak <ryzhak.vladimir@gmail.com>
2019-07-17 02:05:30 +0300

    feat: move from tabs to drawer nav

--- App.js
+++ App.js
@@ -8,45 +8,25 @@
 
 import React, { Component } from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { createAppContainer, createBottomTabNavigator } from 'react-navigation';
+import { createAppContainer, createDrawerNavigator } from 'react-navigation';
 
 import ChatsScreen from 'screens/chats/ChatsScreen';
 import MemesScreen from 'screens/memes/MemesScreen';
 import PlayersScreen from 'screens/players/PlayersScreen';
 
-const TabNavigator = createBottomTabNavigator(
-  {
-    Memes: MemesScreen,
-    Players: PlayersScreen,
-    Chats: ChatsScreen
+const DrawerNavigator = createDrawerNavigator({
+  Memes: {
+    screen: MemesScreen
   },
-  {
-    defaultNavigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused, horizontal, tintColor }) => {
-        const { routeName } = navigation.state;
-        let iconName;
-        if(routeName === 'Memes') iconName = 'md-happy';
-        if(routeName === 'Players') iconName = 'md-football';
-        if(routeName === 'Chats') iconName = 'md-chatbubbles';
-        return <Ionicons name={iconName} size={25} color={tintColor} />;
-      },
-    }),
-    tabBarOptions: {
-      activeBackgroundColor: '#fff',
-      activeTintColor: '#33475c',
-      inactiveBackgroundColor: '#33475c',
-      inactiveTintColor: '#fff',
-      labelStyle: {
-        fontSize: 12
-      },
-      tabStyle: {
-        padding: 4
-      }
-    },
+  Players: {
+    screen: PlayersScreen
+  },
+  Chats: {
+    screen: ChatsScreen
   }
-);
+});
 
-const AppContainer = createAppContainer(TabNavigator);
+const AppContainer = createAppContainer(DrawerNavigator);
 
 type Props = {};
 export default class App extends Component<Props> {
